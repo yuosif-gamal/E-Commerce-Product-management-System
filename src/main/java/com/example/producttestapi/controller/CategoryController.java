194185f7c4a6d2ae43eebdea3ef7ca309932102d
Yousif gamal <56413581+yuosif-gamal@users.noreply.github.com>
2024-07-12 09:37:43 -0700

    adding relationship (category , voucher , Product )

--- src/main/java/com/example/producttestapi/controller/CategoryController.java
+++ src/main/java/com/example/producttestapi/controller/CategoryController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/category")
 public class CategoryController {
 
     private final CategoryService categoryService;
