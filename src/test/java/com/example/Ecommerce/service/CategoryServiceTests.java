9957165748a79ab3d535c6c832f92ff7921f468e
Yousif gamal <56413581+yuosif-gamal@users.noreply.github.com>
2024-08-11 15:10:26 -0700

    refactor and adding test cases

--- src/test/java/com/example/Ecommerce/service/CategoryServiceTests.java
+++ src/test/java/com/example/Ecommerce/service/CategoryServiceTests.java
@@ -1,11 +1,11 @@
 package com.example.Ecommerce.service;
 
+import com.example.Ecommerce.config.CacheConfig;
 import com.example.Ecommerce.dto.CategoryDto;
 import com.example.Ecommerce.entity.Category;
 import com.example.Ecommerce.exception.ResourceNotFoundException;
-import com.example.Ecommerce.mapper.CategoryMapper;
 import com.example.Ecommerce.repository.CategoryRepo;
-import net.bytebuddy.asm.Advice;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,12 +20,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class CategoryServiceTests {
 
+public class CategoryServiceTests {
     @Mock
     private CategoryRepo categoryRepo;
     @InjectMocks
     private CategoryServiceImpl categoryService;
+
 
     @Test
     void testGetCategory_Success() {
@@ -45,7 +46,7 @@ public class CategoryServiceTests {
 
         // Assert
         assertNotNull(result);
-        assertEquals("first category", result.getName());
+        assertEquals(result.getName(), "first category");
         verify(categoryRepo, times(1)).findById(categoryID);
     }
 
@@ -63,7 +64,7 @@ public class CategoryServiceTests {
         });
 
         // Assert that the exception message is correct
-        assertEquals("Category not found with id: " + categoryID, thrownException.getMessage());
+        assertEquals(thrownException.getMessage(), "Category not found with id: " + categoryID);
         verify(categoryRepo, times(1)).findById(categoryID);
     }
 
@@ -73,16 +74,12 @@ public class CategoryServiceTests {
         List<Category> allCategories = new ArrayList<>();
         Category category = new Category();
 
-        Long firstCategoryID = 1L;
         String firstCategoryName = "first category";
-        category.setId(firstCategoryID);
         category.setName(firstCategoryName);
 
         allCategories.add(category);
 
-        firstCategoryID = 2L;
         firstCategoryName = "second category";
-        category.setId(firstCategoryID);
         category.setName(firstCategoryName);
 
         allCategories.add(category);
@@ -95,13 +92,12 @@ public class CategoryServiceTests {
 
         //Assert
         assertNotNull(result);
-        assertEquals(result.size() , 2);
-        assertEquals(result.get(0).getName() , allCategories.get(0).getName());
-        verify(categoryRepo,times(1)).findAll();
+        assertEquals(2, result.size());
+        verify(categoryRepo, times(1)).findAll();
     }
 
     @Test
-    void testGetAllCategories_ReturnEmptyList(){
+    void testGetAllCategories_ReturnEmptyList() {
         List<Category> allCategories = new ArrayList<>();
 
         when(categoryRepo.findAll()).thenReturn(allCategories);
@@ -110,8 +106,144 @@ public class CategoryServiceTests {
         List<CategoryDto> result = categoryService.getAllCategory();
 
         //Assert
-        assertEquals(result.size() , 0);
-        verify(categoryRepo,times(1)).findAll();
+        assertEquals(0, result.size());
+        verify(categoryRepo, times(1)).findAll();
+
+    }
+
+    @Test
+    void testCreateCategory() {
+        String categoryName = "first category";
+        Category category = new Category();
+        category.setName(categoryName);
+
+        when(categoryRepo.save(category)).thenReturn(category);
+
+        Category result = categoryService.createCategory(category);
+
+        assertEquals(category, result);
+        assertEquals(categoryName, result.getName());
+        assertEquals(null, result.getSubCategories());
+        verify(categoryRepo, times(1)).save(category);
+
+    }
+
+    @Test
+    void testDeleteCategory_Success() {
+        String categoryName = "first category";
+        Long categoryID = 1L;
+        Category category = new Category();
+        category.setName(categoryName);
+        category.setId(categoryID);
+
+        doNothing().when(categoryRepo).deleteById(categoryID);
+        when(categoryRepo.findById(categoryID)).thenReturn(Optional.of(category));
+
+        categoryService.deleteCategory(categoryID);
+        verify(categoryRepo, times(1)).deleteById(categoryID);
+        verify(categoryRepo, times(1)).findById(categoryID);
+
+    }
+
+    @Test
+    void testDeleteCategory_Failed() {
+        Long categoryID = 1L;
+
+        when(categoryRepo.findById(categoryID)).thenReturn(Optional.empty());
+
+        ResourceNotFoundException thrownException = assertThrows(ResourceNotFoundException.class, () -> {
+            categoryService.deleteCategory(categoryID);
+        });
+
+        assertEquals(thrownException.getMessage(), "Category not found with id: " + categoryID);
+        verify(categoryRepo, times(0)).deleteById(categoryID);
+        verify(categoryRepo, times(1)).findById(categoryID);
+
+    }
+
+    @Test
+    void testGetAllMainCategories() {
+        Category firstCategory = new Category();
+        firstCategory.setName("first_category");
+
+        Category secondCategory = new Category();
+        secondCategory.setName("second_category");
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(firstCategory);
+        categories.add(secondCategory);
+
+        when(categoryRepo.getAllMainCategories()).thenReturn(categories);
+
+        List<CategoryDto> result = categoryService.getAllMainCategories();
+        List<CategoryDto> result2 = categoryService.getAllMainCategories();
+
+        assertEquals(2, result.size());
+        assertEquals("first_category", result.get(0).getName());
+        assertEquals("second_category", result.get(1).getName());
+        verify(categoryRepo, times(2)).getAllMainCategories();
+    }
+
+    @Test
+    void testGetAllMainCategories_ReturnEmptyList() {
+        List<Category> categories = new ArrayList<>();
+
+        when(categoryRepo.getAllMainCategories()).thenReturn(categories);
+
+        List<CategoryDto> result = categoryService.getAllMainCategories();
+
+        assertEquals(0, result.size());
+        verify(categoryRepo, times(1)).getAllMainCategories();
+    }
+
+    @Test
+    void testGetCategoryChildren_Success() {
+        Category mainCategory = new Category();
+        mainCategory.setName("Main_category");
+        mainCategory.setId(1L);
+
+        Category firstCategory = new Category();
+        firstCategory.setName("first_category");
+        firstCategory.setParentCategory(mainCategory);
+
+        Category secondCategory = new Category();
+        secondCategory.setName("second_category");
+        secondCategory.setParentCategory(mainCategory);
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(firstCategory);
+        categories.add(secondCategory);
+
+        when(categoryRepo.findById(mainCategory.getId())).thenReturn(Optional.of(mainCategory));
+        when(categoryRepo.getCategoryChildren(mainCategory.getId())).thenReturn(categories);
+
+        List<CategoryDto> result = categoryService.getCategoryChildren(mainCategory.getId());
+
+        assertEquals(2, result.size());
+        assertEquals(1, categories.get(0).getParentCategory().getId());
+        assertEquals(1, categories.get(1).getParentCategory().getId());
+        verify(categoryRepo, times(1)).getCategoryChildren(mainCategory.getId());
+        verify(categoryRepo, times(1)).findById(mainCategory.getId());
+    }
+
+
+    @Test
+    void testGetCategoryChildren_Failed() {
+
+        List<Category> categories = new ArrayList<>();
+        Long id = 1L;
+
+        when(categoryRepo.findById(id)).thenReturn(Optional.empty());
+
+
+        ResourceNotFoundException thrownException = assertThrows(ResourceNotFoundException.class,() -> {
+           categoryService.getCategoryChildren(id);
+        });
+
+
+        assertEquals(thrownException.getMessage(), "Category not found with id: " + id);
+        verify(categoryRepo, times(0)).getCategoryChildren(id);
+        verify(categoryRepo, times(1)).findById(id);
 
     }
 
