6853ea1cb44dec50d71bbf67bbfc322900bc2db4
Yousif Gamal <yuosifgamal8@gmail.com>
2024-08-16 19:35:03 +0300

    add feature that you can add one or more products into voucher, add unit test to test that feature.

--- src/main/java/com/example/Ecommerce/service/VoucherService.java
+++ src/main/java/com/example/Ecommerce/service/VoucherService.java
@@ -3,6 +3,9 @@ package com.example.Ecommerce.service;
 import com.example.Ecommerce.dto.VoucherDto;
 import com.example.Ecommerce.entity.Product;
 import com.example.Ecommerce.entity.Voucher;
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 public interface VoucherService {
     VoucherDto createVoucher(VoucherDto voucher);
@@ -12,4 +15,6 @@ public interface VoucherService {
     void deleteVoucher(Long id);
 
     void applyVoucherDiscount(Product product);
+
+    void addProductsToVoucher(Long voucherId, List<Long> productIds);
 }
